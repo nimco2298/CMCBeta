d3bf425fe8311569d88d607691edd372022ca95d
IBUSH001 <ibush001@vplincsci008.ad.csbsju.edu>
2018-04-03 17:16:06 -0500

    Adding in testing stubs for DBController.java

--- tests/controllers/DBControllerTest.java
+++ tests/controllers/DBControllerTest.java
@@ -1,14 +1,30 @@
 package controllers;
 
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
+import controller.DBController;
 
 public class DBControllerTest {
 
+	private DBController db;
+	
+	/**
+	 * This method initializes the information for testing
+	 */
+	@Before
+	public void init()
+	{
+		db = new DBController();
+	}
+	
 	@Test
-	public void test() {
+	public void testGetUniversityForUniversityInDatabase() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
+	public void testGetUniversityForUniversityNotInDatabase() {
+		fail("Not yet implemented");
+	}
 }
