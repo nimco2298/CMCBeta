33bf482bfdc63a351602fc7614cdd4cbd27e9db4
IBUSH001 <ibush001@vplincsci008.ad.csbsju.edu>
2018-04-03 17:00:43 -0500

    Added in DBController.java

---
+++ tests/controllers/DBControllerTest.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DBControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
