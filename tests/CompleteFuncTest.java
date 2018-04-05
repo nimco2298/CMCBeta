58a6124d2eb24eedc8966d9ee0fe027509d240e5
SLAUFERS001 <slaufers001@vplincsci088.ad.csbsju.edu>
2018-04-05 16:14:53 -0500

    Putting in Part2 Test class

---
+++ tests/CompleteFuncTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CompleteFuncTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
