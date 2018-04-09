682c6e5218947b9bd3b8ad9d1948891e815ca4fd
ZBAI001 <zbai001@vplincsci057.ad.csbsju.edu>
2018-04-09 17:25:53 -0500

    recSearch test

--- tests/controllers/SearchControllerTest.java
+++ tests/controllers/SearchControllerTest.java
@@ -107,9 +107,112 @@ public class SearchControllerTest {
 
 	}
 	@Test
-	public void testRecSearch() {
+	public void testRecSearchOrigin() {
 		uList=sc.recSearch(db.getUniversity("BARD"));
+		assertTrue("Should be CLARKSON UNIVERSITY", uList.get(0).getName().equals("CLARKSON UNIVERSITY"));
+		assertTrue("Should be TOURO", uList.get(0).getName().equals("TOURO"));
+		assertTrue("Should be HAMPSHIRE COLLEGE", uList.get(0).getName().equals("HAMPSHIRE COLLEGE"));
+		assertTrue("Should be SUNY PLATTSBURGH", uList.get(0).getName().equals("SUNY PLATTSBURGH"));
+		assertTrue("Should be MANHATTANVILLE COLLEGE", uList.get(0).getName().equals("MANHATTANVILLE COLLEGE"));
+	}
+	
+	@Test
+	public void testRecSearchWithIntDiff() {
+		
+		
+		db.addNewUniversity(new University("Z1",	"NEW YORK","SMALL-CITY",	"PRIVATE",	11000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z2",	"NEW YORK","SMALL-CITY",	"PRIVATE",	12000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z3",	"NEW YORK","SMALL-CITY",	"PRIVATE",	13000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z4",	"NEW YORK","SMALL-CITY",	"PRIVATE",	14000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z5",	"NEW YORK","SMALL-CITY",	"PRIVATE",	15000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		uList=sc.recSearch(db.getUniversity("BARD"));
+		assertTrue("Should be Z1", uList.get(0).getName().equals("Z1"));
+		assertTrue("Should be Z2", uList.get(0).getName().equals("Z2"));
+		assertTrue("Should be Z3", uList.get(0).getName().equals("Z3"));
+		assertTrue("Should be Z4", uList.get(0).getName().equals("Z4"));
+		assertTrue("Should be Z5", uList.get(0).getName().equals("Z5"));
+		
+		db.deleteUniversity(db.getUniversity("Z1"));
+		db.deleteUniversity(db.getUniversity("Z2"));
+		db.deleteUniversity(db.getUniversity("Z3"));
+		db.deleteUniversity(db.getUniversity("Z4"));
+		db.deleteUniversity(db.getUniversity("Z5"));
 		
 	}
+	
 
+	@Test
+	public void testRecSearchWithStringDiff() {
+		
+		emphases.add("LIBERAL-ARTS");
+		db.addNewUniversity(new University("Z1",	"NEW YORK","SMALL-CITY",	"PRIVATE",	11000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z2",	"NEW YORK","SMALL-CITY",	"PRIVATE",	12000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z3",	"NEW YORK","SMALL-CITY",	"PRIVATE",	13000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z4",	"NEW YORK","SMALL-CITY",	"PRIVATE",	14000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z5",	"BEIJING","SMALL-CITY",	"PRIVATE",	10000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		uList=sc.recSearch(db.getUniversity("BARD"));
+		assertTrue("Should be Z1", uList.get(0).getName().equals("Z1"));
+		assertTrue("Should be Z2", uList.get(0).getName().equals("Z2"));
+		assertTrue("Should be Z3", uList.get(0).getName().equals("Z3"));
+		assertTrue("Should be Z4", uList.get(0).getName().equals("Z4"));
+		assertTrue("Should be Z5", uList.get(0).getName().equals("Z5"));
+		
+		db.deleteUniversity(db.getUniversity("Z1"));
+		db.deleteUniversity(db.getUniversity("Z2"));
+		db.deleteUniversity(db.getUniversity("Z3"));
+		db.deleteUniversity(db.getUniversity("Z4"));
+		db.deleteUniversity(db.getUniversity("Z5"));
+		
+	}
+	
+	@Test
+	public void testRecSearchWithMultiIntDiff() {
+		
+		emphases.add("LIBERAL-ARTS");
+		db.addNewUniversity(new University("Z1",	"NEW YORK","SMALL-CITY",	"PRIVATE",	11000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z2",	"NEW YORK","SMALL-CITY",	"PRIVATE",	12000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z3",	"NEW YORK","SMALL-CITY",	"PRIVATE",	13000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z4",	"NEW YORK","SMALL-CITY",	"PRIVATE",	14000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z5",	"NEW YORK","SMALL-CITY",	"PRIVATE",	10000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,5	,	emphases));
+		uList=sc.recSearch(db.getUniversity("BARD"));
+		assertTrue("Should be Z1", uList.get(0).getName().equals("Z1"));
+		assertTrue("Should be Z2", uList.get(0).getName().equals("Z2"));
+		assertTrue("Should be Z3", uList.get(0).getName().equals("Z3"));
+		assertTrue("Should be Z4", uList.get(0).getName().equals("Z4"));
+		assertTrue("Should be Z5", uList.get(0).getName().equals("Z5"));
+		
+		db.deleteUniversity(db.getUniversity("Z1"));
+		db.deleteUniversity(db.getUniversity("Z2"));
+		db.deleteUniversity(db.getUniversity("Z3"));
+		db.deleteUniversity(db.getUniversity("Z4"));
+		db.deleteUniversity(db.getUniversity("Z5"));
+		
+	}
+	@Test
+	public void testRecSearchWithEmphasesDiff() {
+		
+		
+		db.addNewUniversity(new University("Z3",	"NEW YORK","SMALL-CITY",	"PRIVATE",	13000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		db.addNewUniversity(new University("Z4",	"NEW YORK","SMALL-CITY",	"PRIVATE",	14000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		emphases.add("LIBERAL-ARTS");
+		db.addNewUniversity(new University("Z1",	"NEW YORK","SMALL-CITY",	"PRIVATE",	11000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		emphases.add("ARTS");
+		db.addNewUniversity(new University("Z2",	"NEW YORK","SMALL-CITY",	"PRIVATE",	12000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		emphases.remove("LIBERAL-ARTS");
+		db.addNewUniversity(new University("Z5",	"NEW YORK","SMALL-CITY",	"PRIVATE",	15000,	55	,560,	520	,32239,	80	,4000,	70	,30	,3,	4	,4	,	emphases));
+		uList=sc.recSearch(db.getUniversity("BARD"));
+		assertTrue("Should be Z1", uList.get(0).getName().equals("Z1"));
+		assertTrue("Should be Z2", uList.get(0).getName().equals("Z2"));
+		assertTrue("Should be Z3", uList.get(0).getName().equals("Z3"));
+		assertTrue("Should be Z4", uList.get(0).getName().equals("Z4"));
+		assertTrue("Should be Z5", uList.get(0).getName().equals("Z5"));
+		
+		db.deleteUniversity(db.getUniversity("Z1"));
+		db.deleteUniversity(db.getUniversity("Z2"));
+		db.deleteUniversity(db.getUniversity("Z3"));
+		db.deleteUniversity(db.getUniversity("Z4"));
+		db.deleteUniversity(db.getUniversity("Z5"));
+		
+	}
+	
 }
